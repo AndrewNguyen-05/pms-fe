03701ace3e7efad0767311905039f35dd47e9dee
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-16 14:46:58 +0700

    Add custom meta

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Navbar from "@/components/Navbar";
+import Meta from "@/components/Meta";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Meta title={"Project Management System"} />
+    </>
+  );
 }
