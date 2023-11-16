102f7c304447838180253f28ec98b55f86b53974
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-11-16 21:17:18 +0700

    fix nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,46 @@
 import Link from "next/link";
+import NavItem from "./Navitem";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+  const [activeIdx, setActiveIdx] = useState(-1);
+  const items = [
+    { name: "Project", href: "/academic-affair/project/view-project" },
+    {
+      name: "Announcement",
+      href: "/academic-affair/announcement/view-announcement",
+    },
+    {
+      name: "Score",
+      href: "/academic-affair/score/view-score",
+    },
+    { name: "Analysis", href: "/academic-affair/analysis/view-analysis" },
+    { name: "Report", href: "/academic-affair/report/view-report" },
+  ];
+
+  useEffect(() => {
+    let currentPath = router.pathname;
+    items.forEach((item, index) => {
+      if (item.href == currentPath) {
+        setActiveIdx(index);
+        return;
+      }
+    });
+  }, []);
+
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="w-screen flex items-center justify-start px-5 py-1">
           <Link href="/">
-            <button className="font-bold text-[22px] pr-2">
+            <button
+              className="font-bold text-[22px] pr-2"
+              onClick={() => {
+                setActiveIdx(-1);
+              }}
+            >
               <span className="text-sky-500">UIT</span>
               <span className="text-cyan-600">-</span>
               <span className="text-blue-700">PMS</span>
@@ -14,46 +48,18 @@ const Navbar = () => {
           </Link>
 
           <ul className="font-medium text-[17px] flex flex-row p-4 gap-2 items-center rounded-lg bg-white">
-            <li>
-              <Link
-                href="/academic-affair/project/view-project"
-                className="nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:after:w-[100%] hover:text-blue-400 focus:text-blue-700"
-              >
-                Project
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/academic-affair/announcement/view-announcement"
-                className="nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:text-blue-400 focus:text-blue-700"
-              >
-                Announcement
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/academic-affair/score/view-score"
-                className="nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:text-blue-400 focus:text-blue-700"
-              >
-                Score
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/academic-affair/analysis/view-analysis"
-                className="nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:text-blue-400 focus:text-blue-700"
-              >
-                Analysis
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/academic-affair/report/view-report"
-                className="nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:text-blue-400 focus:text-blue-700"
-              >
-                Report
-              </Link>
-            </li>
+            {items.map(({ name, href }, idx) => {
+              return (
+                <div
+                  onClick={() => {
+                    setActiveIdx(idx);
+                  }}
+                  key={idx}
+                >
+                  <NavItem name={name} href={href} active={idx === activeIdx} />
+                </div>
+              );
+            })}
           </ul>
         </div>
       </nav>
---
+++ components/Navitem.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const NavItem = ({ name, href, active }) => {
+  return (
+    <li>
+      <Link
+        href={href}
+        className={
+          "nav-item mx-5 bg-blue-700 rounded md:bg-transparent text-black hover:text-blue-400 focus:text-blue-700 " +
+          (active ? "nav-item-actived" : "")
+        }
+      >
+        {name}
+      </Link>
+    </li>
+  );
+};
+
+export default NavItem;
