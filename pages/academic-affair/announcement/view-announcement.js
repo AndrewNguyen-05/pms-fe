144b8b12a8e72513d555bc67a521c98b335544f3
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-11-17 17:29:55 +0700

    create component for reuseable

--- pages/academic-affair/announcement/view-announcement.js
+++ pages/academic-affair/announcement/view-announcement.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Meta from "@/components/Meta";
+import ButtonCreate from "@/components/ButtonCreate";
+import ButtonDelete from "@/components/ButtonDelete";
+import TableViewItem from "@/components/TableViewItem";
+import SearchBar from "@/components/SearchBar";
+import { useState, useEffect } from "react";
 
 const index = () => {
-  return <div>view announcement</div>;
+  return (
+    <>
+      <Meta title={"View announcement"} />
+      <div className="bg-slate-50 h-full pt-6"></div>
+    </>
+  );
 };
 
 export default index;
