ae8ee1b274bf68db48a1971ef3154f412e669447
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-18 18:13:17 +0700

    Add pagination for announcement

--- pages/academic-affair/announcement/view-announcement.js
+++ pages/academic-affair/announcement/view-announcement.js
@@ -6,29 +6,38 @@ import TableViewItem from "@/components/TableViewItem";
 import SearchBar from "@/components/SearchBar";
 import { getAnnouncementList } from "@/services/announcementServices";
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
 const ViewAnnouncement = () => {
   const [announcementList, setAnnouncementList] = useState([]);
-  const [announcementListCombined, setAnnouncementListCombined] = useState([]);
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentLimit, setCurrentLimit] = useState(10);
+  const [currentOffset, setCurrentOffset] = useState(0);
 
   useEffect(() => {
-    getAnnouncementList().then((announcementList) => {
-      setAnnouncementList(announcementList);
-      setAnnouncementListCombined(
-        announcementList.map((row) => {
-          return {
-            id: row.id,
-            title: row.title,
-            name: row.content,
-            dateCreated: row.dateCreated.slice(0, 10),
-            dateUpdated: row.dateUpdated.slice(0, 10),
-          };
-        })
-      );
+    getAnnouncementData();
+    setCurrentOffset((currentPage - 1) * currentLimit + 1);
+  }, [currentPage]);
 
-      console.log(announcementList);
-    });
-  }, []);
+  async function getAnnouncementData() {
+    let projectsData = await getAnnouncementList(currentPage, currentLimit);
+    setAnnouncementList(
+      projectsData.announcements.map((row) => {
+        return {
+          title: row.title,
+          name: row.content,
+          dateCreated: row.dateCreated.slice(0, 10),
+          dateUpdated: row.dateUpdated.slice(0, 10),
+        };
+      })
+    );
+    setTotalPage(projectsData.totalPage);
+  }
+
+  const handlePageClick = async (event) => {
+    setCurrentPage(+event.selected + 1);
+  };
   return (
     <>
       <Meta title={"View announcement"} />
@@ -45,89 +54,52 @@ const ViewAnnouncement = () => {
         <div className="px-16 py-7 ">
           <TableViewItem
             columnNames={[
-              "bao gio thi duoc 1 ti goi me",
               "Title",
               "Content",
               "Date Created",
               "Last Modified",
               "Action",
             ]}
-            rowList={announcementListCombined}
+            rowList={announcementList}
             editHref="#"
           />
-          <nav
-            className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
-            aria-label="Table navigation"
-          >
+          <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4">
             <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
               Showing{" "}
               <span className="font-semibold text-gray-900 dark:text-white">
-                1-10
+                {currentOffset}-{currentOffset + currentLimit - 1}
               </span>{" "}
               of{" "}
               <span className="font-semibold text-gray-900 dark:text-white">
-                1000
+                {totalPage}
               </span>
             </span>
-            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Previous
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  2
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  aria-current="page"
-                  className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                >
-                  3
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  4
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  5
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Next
-                </a>
-              </li>
-            </ul>
+            <div>
+              {totalPage > 0 && (
+                <ReactPaginate
+                  pageCount={totalPage}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={2}
+                  onPageChange={handlePageClick}
+                  previousLabel="Previous"
+                  nextLabel="Next"
+                  pageClassName=""
+                  pageLinkClassName="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                  previousClassName=""
+                  previousLinkClassName="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+                  nextClassName=""
+                  nextLinkClassName="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+                  breakLabel="..."
+                  breakClassName=""
+                  breakLinkClassName="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                  containerClassName="pagination"
+                  activeClassName=""
+                  activeLinkClassName="bg-blue-100 text-blue-700"
+                  renderOnZeroPageCount={null}
+                  className="inline-flex"
+                />
+              )}
+            </div>
           </nav>
         </div>
       </div>
--- services/announcementServices.js
+++ services/announcementServices.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const getAnnouncementList = async () => {
-  const res = await axios.get("http://localhost:8888/api/v1/announcement/list");
+const getAnnouncementList = async (page, limit) => {
+  const res = await axios.get(
+    `http://localhost:8888/api/v1/announcement/read?page=${page}&&limit=${limit}`
+  );
   if (res && res.data.DT && res.data.EC === 0) {
     return res.data.DT;
   }
