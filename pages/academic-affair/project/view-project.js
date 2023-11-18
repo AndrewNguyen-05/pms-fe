9cb40efb04fe6ba4d3525a9016cf2ace5a05b66e
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-11-18 23:34:13 +0700

    temporary take out modal

--- pages/academic-affair/project/view-project.js
+++ pages/academic-affair/project/view-project.js
@@ -10,6 +10,7 @@ import TableViewItem from "@/components/TableViewItem";
 import SearchBar from "@/components/SearchBar";
 import ReactPaginate from "react-paginate";
 import { toast } from "react-toastify";
+import WarningModal from "@/components/WarningModal";
 
 const ViewProject = () => {
   const [project_list, setProjectList] = useState([]);
@@ -81,79 +82,13 @@ const ViewProject = () => {
       <Meta title={"View project"} />
       <div className="bg-slate-50 min-h-screen pt-6">
         {isModalOpen && (
-          <div
-            tabIndex="-1"
-            className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50"
-            onClick={() => {
-              handleCloseModal();
-            }}
-          >
-            <div
-              className="relative p-4 w-full max-w-md max-h-full"
-              onClick={(event) => event.stopPropagation()}
-            >
-              <div className="relative bg-white rounded-lg shadow">
-                <button
-                  type="button"
-                  className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                  onClick={() => handleCloseModal()}
-                >
-                  <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                    />
-                  </svg>
-                  <span className="sr-only">Close modal</span>
-                </button>
-                <div className="p-4 md:p-5 text-center">
-                  <svg
-                    className="mx-auto mb-4 text-gray-400 w-12 h-12"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                  </svg>
-                  <h3 className="mb-5 text-lg font-normal text-gray-500">
-                    Are you sure you want to delete ?
-                  </h3>
-                  <button
-                    data-modal-hide="popup-modal"
-                    type="button"
-                    className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2"
-                    onClick={() => handleConfirmDelete()}
-                  >
-                    Yes, I'm sure
-                  </button>
-                  <button
-                    data-modal-hide="popup-modal"
-                    type="button"
-                    className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
-                    onClick={() => handleCloseModal()}
-                  >
-                    No, cancel
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <WarningModal
+            question="Are you sure you want to delete ?"
+            btnYesText="Yes, I'm sure"
+            btnNoText="No, cancel"
+            handleConfirmDelete={handleConfirmDelete}
+            handleCloseModal={handleCloseModal}
+          />
         )}
         <div className="flex items-center">
           <div className="px-16">
