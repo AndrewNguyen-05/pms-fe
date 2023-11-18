5b2cadbc3fc29aa81d1c455409d3dee223b2a97c
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-18 19:07:27 +0700

    add modal when cancel the button in create project

--- pages/academic-affair/project/create-project.js
+++ pages/academic-affair/project/create-project.js
@@ -1,10 +1,11 @@
 import Meta from "@/components/Meta";
-import Link from "next/link";
-import { getProjectData, postCreateProject } from "@/services/projectServices";
+import { postCreateProject } from "@/services/projectServices";
 import { getTeacherData } from "@/services/teacherServices";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import ModalWarning from "@/components/ModalWarning";
+import { set } from "lodash";
 
 const CreateProject = () => {
   const [teacher_list, setTeacherList] = useState([]);
@@ -29,6 +30,9 @@ const CreateProject = () => {
   const [projectType, setProjectType] = useState("");
   const [projectRequirement, setProjectRequirement] = useState("");
   const [teacherId, setTeacherId] = useState("");
+
+  //control the state of open modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const router = useRouter();
 
@@ -90,11 +94,94 @@ const CreateProject = () => {
       }
     }
   };
+  console.log(">>> check is modal open before:", isModalOpen);
+  const handleCancelClick = () => {
+    router.push("/academic-affair/project/view-project");
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
       <Meta title={"Create new project"} />
-      <div className="bg-slate-100 h-full py-6">
+      <div className="bg-slate-100 h-full py-6 ">
+        {isModalOpen && (
+          <div
+            tabIndex="-1"
+            className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50"
+            onClick={() => {
+              handleCloseModal();
+            }}
+          >
+            <div
+              className="relative p-4 w-full max-w-md max-h-full"
+              onClick={(event) => event.stopPropagation()}
+            >
+              <div className="relative bg-white rounded-lg shadow">
+                <button
+                  type="button"
+                  className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                  onClick={() => handleCloseModal()}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+                <div className="p-4 md:p-5 text-center">
+                  <svg
+                    className="mx-auto mb-4 text-gray-400 w-12 h-12"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                  <h3 className="mb-5 text-lg font-normal text-gray-500">
+                    Are you sure you want to quit ?
+                  </h3>
+                  <button
+                    data-modal-hide="popup-modal"
+                    type="button"
+                    className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2"
+                    onClick={() => handleCancelClick()}
+                  >
+                    Yes, I'm sure
+                  </button>
+                  <button
+                    data-modal-hide="popup-modal"
+                    type="button"
+                    className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+                    onClick={() => handleCloseModal()}
+                  >
+                    No, cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <section className="bg-white mx-20 rounded-2xl">
           <div className="py-4 px-6">
             <h2 className="mb-4 text-xl font-bold text-gray-900 ">
@@ -278,11 +365,14 @@ const CreateProject = () => {
               </div>
             </div>
             <div className="flex gap-8 justify-end">
-              <Link href="/academic-affair/project/view-project">
-                <button className="items-center px-5 py-2.5 mt-4 text-sm font-medium border-2 border-red-600 text-center text-red-600 bg-white  rounded-lg focus:ring-2 focus:ring-red-200 hover:bg-red-600 hover:text-white hover:border-red-600">
-                  Cancel
-                </button>
-              </Link>
+              <button
+                className="items-center px-5 py-2.5 mt-4 text-sm font-medium border-2 border-red-600 text-center text-red-600 bg-white  rounded-lg focus:ring-2 focus:ring-red-200 hover:bg-red-600 hover:text-white hover:border-red-600"
+                onClick={() => {
+                  setIsModalOpen(true);
+                }}
+              >
+                Cancel
+              </button>
               <button
                 className="items-center px-5 py-2.5 mt-4 text-sm font-medium border-2 border-blue-700 text-center text-blue-700 bg-white  rounded-lg focus:ring-2 focus:ring-blue-200 hover:bg-blue-700 hover:text-white"
                 onClick={() => {
