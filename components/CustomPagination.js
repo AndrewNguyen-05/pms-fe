f43f03b3123ce0cab27d99a87a0758e36d16d960
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-11-19 23:32:17 +0700

    fix pagination UI

---
+++ components/CustomPagination.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CustomPagination = ({ postsPerPage, totalPages, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default CustomPagination;
