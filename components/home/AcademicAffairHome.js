c76ad73e48212243fc79d2cfc329a25f4afc9f50
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-12-04 13:41:11 +0700

    fix academic affair home

--- components/home/AcademicAffairHome.js
+++ components/home/AcademicAffairHome.js
@@ -7,7 +7,7 @@ const AcademicAffairHome = () => {
     <>
       <Meta title={"Project Management System"} />
       <div className="bg-slate-50 w-full h-full flex flex-col items-center">
-        <div className="font-bold text-xl mt-3 text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-2 rounded-lg">
+        <div className="font-bold rounded-full text-xl px-5 py-3 mt-3 text-white bg-gradient-to-r from-blue-600 to-cyan-500 ">
           TRANG HƯỚNG DẪN WEBSITE QUẢN LÝ ĐỒ ÁN 1, 2
         </div>
         <div data-test="main-page-card" className="main-page-card">
@@ -15,23 +15,23 @@ const AcademicAffairHome = () => {
             CÁC CHỨC NĂNG CHÍNH CỦA WEB
           </div>
           <div>
-            <ol className="main-page-card-list list-decimal font-semibold ">
-              <li className="hover:text-gray-500 cursor-pointer">
+            <ol className="main-page-card-list list-decimal font-semibold text-blue-600">
+              <li className="hover:text-sky-500 cursor-pointer">
                 <Link data-test="project-link" href="#project">
                   Quản lý danh sách đồ án
                 </Link>
               </li>
-              <li className="hover:text-gray-500 cursor-pointer">
+              <li className="hover:text-sky-500 cursor-pointer">
                 <Link data-test="announcement-link" href="#announcement">
                   Quản lý danh sách thông báo
                 </Link>
               </li>
-              <li className="hover:text-gray-500 cursor-pointer">
+              <li className="hover:text-sky-500 cursor-pointer">
                 <Link data-test="score-link" href="#score">
                   Quản lý điểm số sinh viên
                 </Link>
               </li>
-              <li className="hover:text-gray-500 cursor-pointer">
+              <li className="hover:text-sky-500 cursor-pointer">
                 <Link data-test="analysis-link" href="#analysis">
                   Quản lý báo cáo, thống kê{" "}
                 </Link>
