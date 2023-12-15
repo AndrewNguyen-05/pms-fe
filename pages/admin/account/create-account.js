001237f09e42847f1fca8e68013cf982fe09d729
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-12-15 13:24:08 +0700

    fix little ui

--- pages/admin/account/create-account.js
+++ pages/admin/account/create-account.js
@@ -34,76 +34,86 @@ const CreateAccount = () => {
   }
 
   return (
-    <div className="h-screen bg-slate-50 px-20 py-8">
-      <div className="text-blue-700 font-bold text-lg ml-3 mb-3">
-        Add new account
-      </div>
-      <div className="flex flex-row flex-wrap gap-3 ">
-        <div className="min-w-max min-h-max max-w-[500px] w-full flex flex-col gap-3">
-          <div className="bg-white shadow-md rounded-xl px-10 py-5 flex flex-col gap-2 ">
-            <div className="mb-1 text-center text-blue-700 font-semibold">
-              Authentication information
+    <div className="h-screen bg-slate-50 py-8 flex justify-center">
+      <div className="w-max h-max bg-white px-9 py-6 rounded-xl shadow-md ">
+        <div className="text-blue-700 font-bold text-lg ml-3 mb-3 text-center">
+          Add new account
+        </div>
+        <div className="flex flex-row gap-2 w-full h-fit">
+          <div className="w-[500px] flex flex-col gap-3">
+            <div className="bg-white shadow-md rounded-xl px-10 py-5 flex flex-col gap-2 ">
+              <div className="mb-1 text-center text-blue-700 font-semibold">
+                Authentication information
+              </div>
+              <InputField
+                title={"Username"}
+                content={username}
+                setContent={setUsername}
+              />
+              <InputField
+                title={"Password"}
+                content={password}
+                setContent={setPassword}
+                inputType="password"
+              />
             </div>
-            <InputField
-              title={"Username"}
-              content={username}
-              setContent={setUsername}
-            />
-            <InputField
-              title={"Password"}
-              content={password}
-              setContent={setPassword}
-              inputType="password"
-            />
-          </div>
 
-          <div className="bg-white shadow-md rounded-xl px-10 py-5 flex flex-col gap-2">
-            <div className="mb-1 text-center text-blue-700 font-semibold">
-              User information
+            <div className="bg-white shadow-md rounded-xl  px-10 py-5 flex flex-col gap-2 ">
+              <div className="mb-1 text-center text-blue-700 font-semibold">
+                User information
+              </div>
+              <InputField title={"Name"} content={name} setContent={setName} />
+              <InputField
+                title={"Email"}
+                content={email}
+                setContent={setEmail}
+              />
+              <InputField
+                title={"Date of birth"}
+                content={dateOfBirth}
+                setContent={setDateOfBirth}
+                inputType="date"
+              />
+              <InputField
+                title={"Phone"}
+                content={phone}
+                setContent={setPhone}
+              />
             </div>
-            <InputField title={"Name"} content={name} setContent={setName} />
-            <InputField title={"Email"} content={email} setContent={setEmail} />
-            <InputField
-              title={"Date of birth"}
-              content={dateOfBirth}
-              setContent={setDateOfBirth}
-              inputType="date"
-            />
-            <InputField title={"Phone"} content={phone} setContent={setPhone} />
+          </div>
+          <div className="bg-white shadow-md rounded-xl w-[500px] px-10 py-5 flex flex-col gap-2">
+            <div className="mb-1 text-center text-blue-700 font-semibold">
+              Role information
+            </div>
+            <div className="flex flex-col">
+              <label>Role</label>
+              <select
+                className="border border-slate-200 rounded px-2 py-1"
+                value={role.value}
+                onChange={(e) => {
+                  setRole({ ...role, value: e.target.value });
+                }}
+              >
+                <option value="">Not selected...</option>
+                <option value="aa">Academic Affair</option>
+                <option value="student">Student</option>
+                <option value="teacher">Teacher</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+            <GetRoleInput role={role} setRole={setRole} />
           </div>
         </div>
-        <div className="bg-white shadow-md rounded-xl min-w-max min-h-max w-full h-full max-w-[500px] px-10 py-5 flex flex-col gap-2">
-          <div className="mb-1 text-center text-blue-700 font-semibold">
-            Role information
-          </div>
-          <div className="flex flex-col">
-            <label>Role</label>
-            <select
-              className="border border-slate-200 rounded px-2 py-1"
-              value={role.value}
-              onChange={(e) => {
-                setRole({ ...role, value: e.target.value });
-              }}
-            >
-              <option value="">Not selected...</option>
-              <option value="aa">Academic Affair</option>
-              <option value="student">Student</option>
-              <option value="teacher">Teacher</option>
-              <option value="admin">Admin</option>
-            </select>
-          </div>
-          <GetRoleInput role={role} setRole={setRole} />
+        <div className="w-full flex justify-end">
+          <button
+            className="bg-blue-700 shadow-md text-white rounded-xl w-full max-w-[150px] h-max p-3 mt-6"
+            onClick={() => {
+              createAccount();
+            }}
+          >
+            Save
+          </button>
         </div>
-      </div>
-      <div>
-        <button
-          className="bg-blue-700 shadow-md text-white rounded-xl w-full max-w-[500px] h-max p-3 mt-6"
-          onClick={() => {
-            createAccount();
-          }}
-        >
-          Save
-        </button>
       </div>
     </div>
   );
